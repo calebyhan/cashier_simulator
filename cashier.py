af4353ec3e548a6560768877f2d5a8afcbc21092
Caleb Han <calebhantech@gmail.com>
2023-04-25 14:17:51 -0400

    commit

--- cashier.py
+++ cashier.py
@@ -4,5 +4,5 @@ import random
 class Cashier:
     def __init__(self):
         self.line = 0
-        self.speed = 1
-        self.items = 0
+        self.speed = random.randint(10, 25)
+        self.customers = []
--- sim.py
+++ sim.py
@@ -1,17 +1,43 @@
 import cashier
+import customer
+
+import random
 
 
 class Sim:
-    def __init__(self, num_cashiers, rate):
-        self.num_cashiers = num_cashiers
-        self.rate = rate
-        self.min = 0
+    def __init__(self, t, num_cashiers, rate):
+        self._num_cashiers = num_cashiers
+        self._rate = rate
+        self.min = t
         self.stats = {"total_queue": []}
-        self.total_queue = 0
+        self._total_queue = 0
 
-        self.cashiers = []
-        for _ in self.num_cashiers:
-            self.cashiers.append(cashier.Cashier())
+        self._cashiers = []
+        for _ in range(self._num_cashiers):
+            self._cashiers.append(cashier.Cashier())
+
+    def _update_stats(self):
+        self.stats["total_queue"].append(self._total_queue)
 
     def run(self):
-        self.min += 1
+        while self.min >= 0:
+            self.min -= 1
+
+            sorted_cashiers = sorted(self._cashiers, key=lambda x: x.line)
+
+            if random.random() < self._rate:
+                for _ in range(random.randint(1, 4)):
+                    sorted_cashiers[0].line += 1
+                    sorted_cashiers[0].customers.append(customer.Customer())
+                    self._total_queue += 1
+
+            for staff in self._cashiers:
+                if len(staff.customers) > 0:
+                    if staff.customers[0].items <= 0:
+                        staff.customers.pop(0)
+                        staff.line -= 1
+                        self._total_queue -= 1
+                    else:
+                        staff.customers[0].items -= staff.speed
+
+            self._update_stats()
